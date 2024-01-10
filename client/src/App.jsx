4a9875dd30978923fcea8ab60e48eb0927cebb46
Frango113 <frangoat38@aol.com>
2024-01-10 15:04:36 -0300

    agregado componente Cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import logo from './utils/img/logo.png'
-import './App.css'
+import logo from "./utils/img/logo.png";
+import "./App.css";
 
 const App = () => {
-
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-      Hello world! Tailwind installed! ✨
+        Hello world! Tailwind installed! ✨
       </h1>
       <img src={logo} alt="Trip In Sight" />
       <Routes>
-        <Route path='/'  />
-        <Route path='/login' />
-        <Route path='/aboutus' />
-        <Route path='/guides' />
-        <Route path='/tours' />
-        <Route path='/profile' />
-        <Route path='/tour' />
-        <Route path='/guide' />
+        <Route path="/" />
+        <Route path="/login" />
+        <Route path="/aboutus" />
+        <Route path="/guides" />
+        <Route path="/tours" />
+        <Route path="/profile" />
+        <Route path="/tour" />
+        <Route path="/guide" />
+        <Route path="/cart" />
+        <Route path="/admin" />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
