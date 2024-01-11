9162364650bf4bc6dd01c56f7cbc71e65a9df0a9
Dana Jacob Casanoves <danajacobc@gmail.com>
2024-01-11 12:13:02 -0300

    auth0 - configured

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,19 @@ import App from './App.jsx'
 import {Provider} from 'react-redux';
 import {store} from './redux/store.js';
 import {BrowserRouter} from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.AUTH0_DOMAIN;
+const clientId = process.env.AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
    <React.StrictMode>
+    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
     <BrowserRouter>
      <App />
     </BrowserRouter>
+    </Auth0Provider>
    </React.StrictMode>,
   </Provider>
 )
