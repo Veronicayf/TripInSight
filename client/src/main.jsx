816966958569673df9f06dbd33dca6a343e20acd
Dana Jacob Casanoves <danajacobc@gmail.com>
2024-01-11 12:52:56 -0300

    add login and logout buttons

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,28 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {Provider} from 'react-redux';
-import {store} from './redux/store.js';
-import {BrowserRouter} from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.AUTH0_DOMAIN;
 const clientId = process.env.AUTH0_CLIENT_ID;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-   <React.StrictMode>
-    <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-    </Auth0Provider>
-   </React.StrictMode>,
-  </Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
+    <Provider store={store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+      ,
+    </Provider>
+  </Auth0Provider>
+);
