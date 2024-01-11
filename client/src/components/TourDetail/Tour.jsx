34a52f275dae8e69baf735f45e61a23a7e8f6875
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2024-01-11 15:17:32 -0300

    Price Section Finish

--- client/src/components/TourDetail/Tour.jsx
+++ client/src/components/TourDetail/Tour.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import tourStyle from './tour.module.css'
 import TourInfoItem from '../TourInfoItem/TourInfoItem';
+import Buysection from '../Buysection/Buysection';
 import PhotoSection from '../PhotoSection/PhotoSection';
 import GuideSection from '../TourDetailGuideSection/GuideSection';
 import ImagePrincipal from '../../assets/img/Caballo1.jpg'
@@ -18,8 +19,14 @@ import IconIncludes from '../../assets/icons/includesIcon.png'
 import iconPhoto from '../../assets/icons/photosIcon.png'
 
 const Tour = () => {
+
+
+
+
+
+
   return (
-    <main className=''>
+    <main >
         {/* NavBar Section */}
         <section className='w-full flex justify-center bg-transparent'>
             <p>Aca va a la navBar</p>
@@ -44,21 +51,24 @@ const Tour = () => {
                 </ul>
             </div>
         </section>
+        <section >
+        <Buysection/>
+        </section>
 {/*-------------- Info Del Tour --------------*/}
 {/*-------------- Page Nav Bar--------------*/}
         <section className='px-4 flex w-3/4 h-14 bg-gray-500 justify-around items-center'>
             <div className=''>
-                <button type="button" class="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800">
+                <button type="button" className="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800">
                     <Link to='#description'>Description</Link>
                 </button>
             </div>
             <div>
-            <button type="button" class="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800">
+            <button type="button" className="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800">
                     <Link to='#photos'>Photos</Link>
                 </button>
             </div>
             <div>
-            <button type="button" class="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800">
+            <button type="button" className="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800">
                     <Link to='#guide'>Guide</Link>
                 </button>
             </div>
