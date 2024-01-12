c831bcd147b37482be745f12b5cc0980a3d732c3
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2024-01-12 12:03:55 -0300

    Corrigiendo error en landing page

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -6,7 +6,9 @@ import ImageProvisoria1 from "../../assets/img/ciervo1.jpg";
 import ImageProvisoria2 from "../../assets/img/paisaje1.jpg";
 import ImageProvisoria3 from "../../assets/img/paisaje2.jpg";
 import IconGoTo from "../../assets/icons/gotoIcon.png";
+import IconSale from '../../assets/icons/IconSale.png'
 import Carousel from "../../components/Carrusel/Carousel";
+
 
 const Home = () => {
   return (
@@ -62,6 +64,49 @@ const Home = () => {
           </div>
         </div>
       </section>
+      {/*<-- Subscribe section -->*/}
+      <section>
+            <div className="bg-bg-landscape bg-cover text-white flex p-24">
+            {/<-- Left -->/ }
+                <div className="flex flex-col w-1/2 justify-around items-center h-96">
+                    <div className="w-5/6 h-5/6 m-auto p-4 bg-darkgreen-bg opacity-75 flex flex-col rounded-3xl justify-around">
+                        <div className="flex">
+                            <div className="w-2/3 flex flex-col justify-center">
+                                <b className="text-4xl opacity-100">Discover Especial Deals!</b>
+                                <p className="text-2xl">Make sure to check out these special promotions</p>
+                            </div>
+                            <div className="flex items-center w-1/3">
+                            <i><img src={IconSale} alt="Sales Icon" className="h-38 w-36" /></i>
+                            </div>
+                        </div>
+                        <div className="h-12 flex justify-center">
+                            <button className="h-12 w-44 bg-white text-black rounded-3xl hover:bg-primary">
+                                <Link><b>See Tours</b></Link>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/<-- right -->/ }
+                <div className="flex flex-col w-1/2 justify-around items-center h-96">
+                    <div className="w-5/6 h-5/6 m-auto p-4 bg-primary opacity-75  flex flex-col rounded-3xl justify-around">
+                        <div className="flex">
+                            <div className="w-2/3 flex flex-col justify-center">
+                                <b className="text-3xl opacity-100">Don´t miss a thing</b>
+                                <p className="text-xl">Get update to special deals and exclusive offers</p>
+                            </div>
+                            <div className="flex items-center w-1/3">
+                            <i><img src={IconSale} alt="Sales Icon" className="h-38 w-36" /></i>
+                            </div>
+                        </div>
+                        <div className="h-12 flex justify-center">
+                            <button className="h-12 w-44 bg-white text-black rounded-3xl hover:bg-primary">
+                                <Link><b>See Tours</b></Link>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
   );
 };
