f688e4ec79b7b25d561933c803ce50cca112ffc6
Dana Jacob Casanoves <danajacobc@gmail.com>
2024-01-13 15:34:35 -0300

    users register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,16 @@ import Footer from "./components/FootBar/FootBar";
 import Login from "./views/Login/Login";
 import Home from "./views/Home/Home";
 import AdminPanel from "./views/AdminPanel/AdminPanel";
+import Register from "./components/Register/Register";
+
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
+
+  //dana
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
   return (
     <div>
       <NavBar />
@@ -25,6 +33,8 @@ const App = () => {
         <Route path="/guide" />
         <Route path="/cart" />
         <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/register" element={<Register setAuth={setAuth} />} />
+
       </Routes>
       <Footer />
     </div>
