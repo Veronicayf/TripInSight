d3e336493748a5772b3df1d0ffbeee8088bbadab
Dana Jacob Casanoves <danajacobc@gmail.com>
2024-01-17 15:00:17 -0300

    seachbar cambios

--- client/src/components/NavBar/SearchBar/SearchBar.jsx
+++ client/src/components/NavBar/SearchBar/SearchBar.jsx
@@ -1,5 +1,36 @@
+import searchIcon from '../../../assets/icons/searchIcon.png'
+import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { getTourName, searchTourTags } from '../../../redux/tourStore/toursActions';
 
+const SearchBar = () => {
+    const [search, setSearch] = useState('');
+    const dispatch = useDispatch();
 
+    const handleChange = (event) => {
+        setSearch(event.target.value);
+        dispatch(getTourName(search));
+        //dispatch(searchTourTags(search)) //o se despacha por name o por tags. tener ruta que busque por ambos.
+      };
+
+    const handleButtonSubmit = () => {
+        dispatch(getTourName(search));
+        setSearch('');
+    }
+
+    return (
+        <div className='flex flex-row items-center text-black'>
+            <input className='rounded-lg p-1' type="search" placeholder="Search tour..." onChange={handleChange}/>
+            {/* <img src={search} alt='Search Tour'/> */}
+            
+            <button onClick={handleButtonSubmit}><img src={searchIcon} alt='Search Tour'/></button>
+        </div>
+    )
+}
+
+export default SearchBar;
+
+//
 // import searchIcon from '../../../assets/icons/searchIcon.png'
 // import { useState } from "react";
 // import { useDispatch } from 'react-redux';
@@ -9,10 +40,22 @@
 //     const [search, setSearch] = useState('');
 //     const dispatch = useDispatch();
 
-//     const handleChange = (event) => {
+//     // Función de aplazamiento con un retraso de 500ms
+//     const debounce = (fn, delay) => {
+//         let debounceTimer;
+//         return function() {
+//             const context = this;
+//             const args = arguments;
+//             clearTimeout(debounceTimer);
+//             debounceTimer = setTimeout(() => fn.apply(context, args), delay);
+//         }
+//     }
+
+//     // Versión aplazada de handleChange
+//     const debouncedHandleChange = debounce(event => {
 //         setSearch(event.target.value);
-//         dispatch(getTourName(search));
-//       };
+//         dispatch(getTourName(event.target.value));
+//     }, 500);
 
 //     const handleButtonSubmit = () => {
 //         dispatch(getTourName(search));
@@ -21,9 +64,7 @@
 
 //     return (
 //         <div className='flex flex-row items-center text-black'>
-//             <input className='rounded-lg p-1' type="search" placeholder="Search tour..." onChange={handleChange}/>
-//             {/* <img src={search} alt='Search Tour'/> */}
-            
+//             <input className='rounded-lg p-1' type="search" placeholder="Search tour..." onChange={debouncedHandleChange}/>
 //             <button onClick={handleButtonSubmit}><img src={searchIcon} alt='Search Tour'/></button>
 //         </div>
 //     )
--- client/src/redux/tourStore/toursActions.js
+++ client/src/redux/tourStore/toursActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getAllTours, getTourById, searchTourByName } from "./toursSlice";
+import { getAllTours, getTourById, searchTourByName, searchTourByTags, } from "./toursSlice";
 
 export const getAllT = () => {
   return async (dispatch) => {
@@ -18,7 +18,14 @@ export const getAllT = () => {
 
 export const getTourName = (tour) => {
   return async (dispatch) => {
-    let {data} = await axios.get(`http://localhost:4000/tours/tags?tags=${tour}`);
+    let {data} = await axios.get(`http://localhost:4000/tours/nameTour?nameTour=${tour}`);
     return dispatch(searchTourByName(data));
+  }
+}
+
+export const searchTourTags = (tour) => {
+  return async (dispatch) => {
+    let {data} = await axios.get(`http://localhost:4000/tours/tags?tags=${tour}`);
+    return dispatch(searchTourByTags(data));
   }
 }
