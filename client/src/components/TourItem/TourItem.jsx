edc888fa243d5eb708bd5be21dc17a978fa0f757
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2024-01-18 09:28:05 -0300

    Tour detail en Dashboard terminado

--- client/src/components/TourItem/TourItem.jsx
+++ client/src/components/TourItem/TourItem.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import iconmenu from '../../assets/icons/IconMenuVertical.png';
 
 const TourItem = ({ tourInfo }) => {
+
+    const statusStyle = {
+        backgroundColor: tourInfo.status === 'true' ? 'green' : 'red',
+        color: 'white',
+        padding: '5px 10px',
+        borderRadius: '5px',
+    };
+
     return (
     <div id='Item' className='flex flex-row w-full justify-between py-4'>
         <div className='w-2/6 flex flex-row items-center justify-around'>
@@ -18,7 +26,7 @@ const TourItem = ({ tourInfo }) => {
             <b>${tourInfo.price}</b>
         </div>
         <div className='w-1/6 flex flex-row items-center justify-around'>
-            <b>{tourInfo.status}</b>
+        <div style={statusStyle}><b>{tourInfo.status}</b></div>
         </div>
         <div className='w-1/6 flex flex-row items-center justify-around'>
             <button className='h-10 w-10 bg-primary rounded-2xl'>
