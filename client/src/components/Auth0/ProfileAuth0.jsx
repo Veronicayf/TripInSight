e3d0e57b51c46593d2283444907cc62a541a217e
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2024-01-20 18:21:27 -0300

    dopmenu terminado

--- client/src/components/Auth0/ProfileAuth0.jsx
+++ client/src/components/Auth0/ProfileAuth0.jsx
@@ -1,12 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import DropMenu from "../DropMenu/DropMenu";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   const [open, setOpen] = useState(false)
 
-  console.log(user)
+  const options = [
+    { label: 'Profile', to: `/favorites` },
+    { label: 'Settings', to: `/profile/:id` },
+  ];
+
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -17,8 +23,8 @@ const Profile = () => {
         <img className="rounded-full size-16 cursor-pointer" src={user.picture} alt={user.name} onClick={()=>setOpen(!open)} />
         <h2>{user.name}</h2>
         {/* <p>{user.email}</p> */}
-        {open && <DropMenu/>}
-        
+        {open && <DropMenu options={options} />
+        }
       </div>
     )
   );
--- client/src/components/DropMenu/DropMenu.jsx
+++ client/src/components/DropMenu/DropMenu.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom';
 
-const DropMenu = () => {
+const DropMenu = ({ options }) => {
 
-  const menus=['Profile','Settings']
+  
+
+
   
   return (
-    <div className='bg-white text-black p-4 w-52 shadow-lg absolute -left-14 top-24 '>
+    <div className='bg-white text-black p-4 w-52 shadow-lg absolute -left-14 top-24'>
       <ul>
-          {menus.map((menu)=>(
-            <li className='p-2 text-lg cursor-pointer rounded hover:bg-seconday-text' key={menu}>{menu}</li>
-          ))}
+        {options.map((option) => (
+          <li className='p-2 text-lg cursor-pointer rounded hover:bg-seconday-text' key={option.label}>
+            {option.to ? (
+              <Link to={option.to}>{option.label}</Link>
+            ) : (
+              <span>{option.label}</span>
+            )}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default DropMenu
