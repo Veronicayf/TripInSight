8f45839eed0fb6dc0b5c0380c265167ee98f4249
Frango113 <frangoat38@aol.com>
2024-01-21 19:42:46 -0300

    for deploy

--- client/src/redux/tourStore/toursActions.js
+++ client/src/redux/tourStore/toursActions.js
@@ -13,14 +13,16 @@ import {
 
 export const getAllT = () => {
   return async (dispatch) => {
-    let json = await axios.get("http://localhost:4000/tours");
+    let json = await axios.get("https://tripinsight.onrender.com/tours");
     return dispatch(getAllTours(json.data));
   };
 };
 
 export const getTourId = (id) => {
   return async (dispatch) => {
-    let { data } = await axios.get(`http://localhost:4000/tours/${id}`);
+    let { data } = await axios.get(
+      `https://tripinsight.onrender.com/tours/${id}`
+    );
     return dispatch(getTourById(data));
   };
 };
@@ -28,7 +30,7 @@ export const getTourId = (id) => {
 export const getTourName = (tour) => {
   return async (dispatch) => {
     let { data } = await axios.get(
-      `http://localhost:4000/tours/nameTour?nameTour=${tour}`
+      `https://tripinsight.onrender.com/tours/nameTour?nameTour=${tour}`
     );
     return dispatch(searchTourByName(data));
   };
@@ -40,7 +42,7 @@ export const postTourAction = (posteoTour) => {
   return async (dispatch) => {
     try {
       const response = await axios.post(
-        "http://localhost:4000/tours",
+        "https://tripinsight.onrender.com/tours",
         posteoTour
       );
       dispatch(postTour(response.data));
@@ -53,7 +55,7 @@ export const postTourAction = (posteoTour) => {
 export const searchTourTags = (tour) => {
   return async (dispatch) => {
     let { data } = await axios.get(
-      `http://localhost:4000/tours/tags?tags=${tour}`
+      `https://tripinsight.onrender.com/tours/tags?tags=${tour}`
     );
     return dispatch(searchTourByTags(data));
   };
@@ -61,35 +63,34 @@ export const searchTourTags = (tour) => {
 
 export const filterContinent = (tour) => {
   return async (dispatch) => {
-    if(tour === 'all') {
-      return dispatch(filterTourByContinent('all'));
+    if (tour === "all") {
+      return dispatch(filterTourByContinent("all"));
     }
     try {
-
-      let {data} = await axios.get(`http://localhost:4000/tours/continent?continent=${tour}`);
+      let { data } = await axios.get(
+        `https://tripinsight.onrender.com/tours/continent?continent=${tour}`
+      );
       return dispatch(filterTourByContinent(data));
-
     } catch (error) {
-      console.error('Error filter continent:', error)
+      console.error("Error filter continent:", error);
     }
-  }
-}
+  };
+};
 
 export const sortToursPrice = () => {
   return (dispatch) => {
     dispatch(sortToursByPrice());
-  }
-
-}
+  };
+};
 
 export const addTourCart = (tour) => {
   return (dispatch) => {
     dispatch(addTourCartReducer(tour));
-  }
-}
+  };
+};
 
 export const cartTotal = (price) => {
   return (dispatch) => {
     dispatch(cartTotalReducer(price));
-  }
-}
+  };
+};
--- client/src/redux/userStore/usersActions.js
+++ client/src/redux/userStore/usersActions.js
@@ -1,5 +1,10 @@
-import axios from 'axios'
-import { getAllUsers, getUserDetail, loggedUserReducer, updateUserReducer } from './usersSlice'
+import axios from "axios";
+import {
+  getAllUsers,
+  getUserDetail,
+  loggedUserReducer,
+  updateUserReducer,
+} from "./usersSlice";
 
 //ver error -> sequelizedatabase error
 // export const getUsers = () => {
@@ -10,30 +15,38 @@ import { getAllUsers, getUserDetail, loggedUserReducer, updateUserReducer } from
 // };
 
 export const getUserId = (id) => {
-    return async (dispatch) => {
-      let {data} = await axios.get(`http://localhost:4000/user/getuser/${id}`);
-      return dispatch(getUserDetail(data));
-    };
+  return async (dispatch) => {
+    let { data } = await axios.get(
+      `https://tripinsight.onrender.com/user/getuser/${id}`
+    );
+    return dispatch(getUserDetail(data));
   };
+};
 
 export const loggedUser = (user) => {
   return async (dispatch) => {
-   try { 
-    let response = await axios.post("http://localhost:4000/user", user);
-    return dispatch(loggedUserReducer(response.data));
-  } catch(error) {
-    console.log(error.response.data);
-  }
-  }
+    try {
+      let response = await axios.post(
+        "https://tripinsight.onrender.com/user",
+        user
+      );
+      return dispatch(loggedUserReducer(response.data));
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
 };
 
 export const updateUser = (userData) => {
   return async (dispatch) => {
-   try { 
-    let response = await axios.put("http://localhost:4000/user/updateuser", userData);
-    return dispatch(updateUserReducer(response.data));
-  } catch(error) {
-    console.log(error.response.data);
-  }
-  }
+    try {
+      let response = await axios.put(
+        "https://tripinsight.onrender.com/user/updateuser",
+        userData
+      );
+      return dispatch(updateUserReducer(response.data));
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
 };
