9d0dd7cba342b4748787bf33555dd9d4f274dfa9
Dana Jacob Casanoves <danajacobc@gmail.com>
2024-01-22 16:37:54 -0300

    guide detail - tours with guide

--- client/src/views/GuideDetail/Guide.jsx
+++ client/src/views/GuideDetail/Guide.jsx
@@ -1,64 +1,88 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Carousel from '../../components/Carrusel/Carousel'
+import React from "react";
+import { Link } from "react-router-dom";
+import Carousel from "../../components/Carrusel/Carousel";
 import ImageProvisoria1 from "../../assets/img/ciervo1.jpg";
 import ImageProvisoria2 from "../../assets/img/paisaje1.jpg";
 import ImageProvisoria3 from "../../assets/img/paisaje2.jpg";
-import IconBirthday from '../../assets/icons/IconBirthday.png'
-import IconCountry from '../../assets/icons/IconCountry.png'
-import IconHome from '../../assets/icons/IconHome.png'
-import { useSelector } from 'react-redux';
-
-
+import IconBirthday from "../../assets/icons/IconBirthday.png";
+import IconCountry from "../../assets/icons/IconCountry.png";
+import IconHome from "../../assets/icons/IconHome.png";
+import { useSelector } from "react-redux";
+import TourCard from "../../components/TourCard/TourCard";
 
 const GuideDetail = () => {
   const guideDetail = useSelector((state) => state.guide.detail);
+  const tours = useSelector((state) => state.tour.tours);
+  const tourGuide = tours.filter((tour) => tour.guideId === guideDetail.id);
 
-  const place = "algunlugar"
   return (
     <div className=" font-Nunito">
-        <Carousel
+      <Carousel
         images={[ImageProvisoria1, ImageProvisoria2, ImageProvisoria3]}
       />
-      <div className='w-full flex flex-row p-4'>
-        <div className='w-2/3 px-9'>
-            <div className=' flex flex-col'>
-            <div className='flex justify-center flex-row py-9'>
-                <b className='text-5xl'>{guideDetail.forename}</b> 
-                <b className='text-5xl text-primary pl-4'>{guideDetail.surname}</b> </div>
+      <div className="w-full flex flex-row p-4">
+        <div className="w-2/3 px-9">
+          <div className=" flex flex-col">
+            <div className="flex justify-center flex-row py-9">
+              <b className="text-5xl">{guideDetail.forename}</b>
+              <b className="text-5xl text-primary pl-4">
+                {guideDetail.surname}
+              </b>{" "}
             </div>
-            <div className='flex justify-center items-center'>
-                <p className=' w-1/2'>{guideDetail.biography}</p>
+          </div>
+          <div className="flex justify-center items-center">
+            <p className="w-1/2">{guideDetail.biography}</p>
+          </div>
+          <div className="flex text-center justify-around items-center ">
+            <div className="flex text-center items-center py-8">
+              <i>
+                <img className="h-12" src={IconBirthday} alt="" />
+              </i>
+              <b>{guideDetail.birthDate}</b>
             </div>
-            <div className='flex text-center justify-around items-center '>
-              <div className='flex text-center items-center py-8'>
-                <i ><img className='h-12' src={IconBirthday} alt="" /></i>
-                <b>{ guideDetail.birthDate}</b>
-              </div>
-              <div className='flex text-center items-center'>
-                <i ><img className='h-12' src={IconCountry} alt="" /></i>
-                <b>{ guideDetail.nationality}</b>
-              </div>
+            <div className="flex text-center items-center">
+              <i>
+                <img className="h-12" src={IconCountry} alt="" />
+              </i>
+              <b>{guideDetail.nationality}</b>
             </div>
-            <div className='flex flex-row p-4 justify-around items-center'>
+          </div>
+          {/* <div className='flex flex-row p-4 justify-around items-center'>
               <div>
                 <button className='bg-primary w-48 h-14 rounded-3xl flex justify-center items-center text-white text-xl font-bold border-black border-2  hover:bg-btn-hover transition hover:duration-300 hover:scale-110 ease-in-out '> <img src={IconHome} alt="Icon home" className='h-10' /><Link to={"/"}>Home</Link></button>
               </div>
-              {/* <div>
+              <div>
               <button className='bg-white w-48 h-14 rounded-3xl flex justify-center items-center text-primary border-black border-2  text-xl font-bold hover:bg-btn-hover transition hover:duration-300 hover:scale-110 ease-in-out  '><img src="" alt="" /> <Link to={place}>Guide's Tours</Link>
               </button>
-              </div> */}
-            </div>
+              </div>
+            </div> */}
         </div>
 
         <div>
-            <div>
-                <img className='h-1/2' src={ guideDetail.image} alt="" />
-            </div>
+          <div className="">
+            <img className="h-1/2" src={guideDetail.image} alt="" />
+          </div>
         </div>
       </div>
+      {/* Tours por guía */}
+
+      <div className="flex flex-col w-full justify-around items-center">
+        <div className="flex justify-center flex-row">
+          <b className="text-5xl">Tours with</b>
+          <b className="text-5xl text-primary pl-4">{guideDetail.forename}</b>
+        </div>
+        <div className="flex justify-center flex-row gap-5 py-9">
+        {tourGuide && tourGuide.length > 0 ? (
+          tourGuide?.map((tour, index) => <TourCard key={index} tour={tour} />)
+        ) : (
+          <div className="flex flex-col justify-center items-center p-5 gap-5">
+            <b>No tours assigned at this time...</b>
+          </div>
+        )}
+        </div>    
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default GuideDetail;
