2d0170dd2207314dfb1e1e24c0c1c21e55531474
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2024-01-23 23:50:12 -0300

    profile section terminada, y conectada al back

--- client/src/views/ProfileFavs/ProfileFavs.jsx
+++ client/src/views/ProfileFavs/ProfileFavs.jsx
@@ -7,16 +7,17 @@ import { Link } from "react-router-dom";
 import ReviewSection from "../../components/ReviewSection/ReviewSection";
 import TicketSection from "../../components/TicketSection/TicketSection";
 import Pagination from "../../components/Pagination/Pagination";
+import { useDispatch, useSelector } from "react-redux";
 
 const ticketData = [
   {
-    id: "1252352312342",
+    id: "125232312342",
     tourname: " Cachorritas en chaco con tomy",
     price: "3500",
     date: "22/01/2024",
   },
   {
-    id: "63463412512",
+    id: "634634125",
     tourname: " aureola boreal en chaco con tomy",
     price: "6500",
     date: "16/01/2024",
@@ -28,7 +29,7 @@ const ticketData = [
     date: "30/02/2023",
   },
   {
-    id: "1252352312342",
+    id: "1252352342",
     tourname: "Snowboarding en dunas de tierra en chaco, con tomy",
     price: "1200",
     date: "13/01/2024",
@@ -42,9 +43,12 @@ const ticketData = [
 ];
 
 const ProfileFavs = () => {
+
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.userProfile);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10; 
-
+  console.log(profile);
   // Función para manejar el cambio de página
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -76,7 +80,38 @@ const ProfileFavs = () => {
                 <div className="w-1/2">
                   <div className="flex justify-center"><b>My</b><b className="text-primary">Profile</b></div>
                   <div className="w-full flex items-center flex-col border-2 border-seconday-text rounded-xl px-10 my-4">
-                    
+                    <div className="w-full flex flex-col justify-center" >
+                      <div className="w-full flex flex-col items-center justify-center py-8">
+                        <img className="rounded-full h-36 w-36" src={profile.image} alt={profile.name} />
+                        <b className=" py-4">{profile.email}</b>
+                      </div>
+                      <div className=" w-full flex flex-row justify-around items-center py-4">
+                        <div className=" w-1/2 flex flex-col items-center">
+                          <b>Name:</b>
+                          <b>{profile.name}</b>
+                        </div>
+                        <div className=" w-1/2 flex flex-col items-center">
+                            <b>Phone Number:</b>
+                            <b>{profile.phoneNumber}</b>
+                        </div>
+                      </div>
+                      <div className=" w-full flex flex-row justify-around items-center py-4 ">
+                        <div className=" w-1/2 flex flex-col items-center">
+                          <b>Birth Date:</b>
+                          <b>{profile.birthDate}</b>
+                        </div>
+                        <div className=" w-1/2 flex flex-col items-center">
+                            <b>Nationality:</b>
+                            <b>{profile.nationality}</b>
+                        </div>
+                      </div>
+                      <div className="flex justify-center py-2">
+                          <button className="bg-primary text-white py-2 px-4 rounded-md hover:bg-btn-hover">
+                            <Link to={`/profilefavs/${profile.name}`}><b>Edit Profile</b></Link>
+                          </button>
+                      </div>
+                      
+                    </div>
 
                   </div>
                 </div>
