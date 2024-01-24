2b54cda9ce5962a2d12230a1b18ad97e1086e19d
Tomas Gabriel Aquino Bajusz <tomasaquinogabi@gmail.com>
2024-01-23 23:58:16 -0300

    modularizado

--- client/src/views/ProfileFavs/ProfileFavs.jsx
+++ client/src/views/ProfileFavs/ProfileFavs.jsx
@@ -8,6 +8,7 @@ import ReviewSection from "../../components/ReviewSection/ReviewSection";
 import TicketSection from "../../components/TicketSection/TicketSection";
 import Pagination from "../../components/Pagination/Pagination";
 import { useDispatch, useSelector } from "react-redux";
+import ProfileView from "../../components/ProfileView/ProfileView";
 
 const ticketData = [
   {
@@ -43,11 +44,10 @@ const ticketData = [
 ];
 
 const ProfileFavs = () => {
-
   const dispatch = useDispatch();
   const profile = useSelector((state) => state.user.userProfile);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10; 
+  const itemsPerPage = 10;
   console.log(profile);
   // Función para manejar el cambio de página
   const handlePageChange = (pageNumber) => {
@@ -61,60 +61,27 @@ const ProfileFavs = () => {
       <div>
         <div>
           <ReviewSection />
-          <div className="w-full flex items-center flex-col border-2 border-seconday-text rounded-xl px-10 my-4">
-            <div className="text-3xl my-4">
+          <div className="w-full flex items-center flex-col border-2 border-seconday-text rounded-xl px-10">
+            <div className="text-3xl py-4">
               <b>My</b> <b className="text-primary">Things</b>
             </div>
             <div className="w-full flex flex-row my-8">
               <div className="w-full flex flex-row">
                 <div className="w-1/2">
-                  <div className="flex justify-center"><b>My </b> <b className=" text-primary">Tickets</b></div>
+                  <div className="flex justify-center">
+                    <b>My </b> <b className=" text-primary">Tickets</b>
+                  </div>
                   <TicketSection ticketData={ticketData} />
                   <Pagination
-                  itemPerPage={itemsPerPage}
-                  totalItems={ticketData.length}
-                  paginate={handlePageChange}
+                    itemPerPage={itemsPerPage}
+                    totalItems={ticketData.length}
+                    paginate={handlePageChange}
                   />
                 </div>
-                
-                <div className="w-1/2">
-                  <div className="flex justify-center"><b>My</b><b className="text-primary">Profile</b></div>
-                  <div className="w-full flex items-center flex-col border-2 border-seconday-text rounded-xl px-10 my-4">
-                    <div className="w-full flex flex-col justify-center" >
-                      <div className="w-full flex flex-col items-center justify-center py-8">
-                        <img className="rounded-full h-36 w-36" src={profile.image} alt={profile.name} />
-                        <b className=" py-4">{profile.email}</b>
-                      </div>
-                      <div className=" w-full flex flex-row justify-around items-center py-4">
-                        <div className=" w-1/2 flex flex-col items-center">
-                          <b>Name:</b>
-                          <b>{profile.name}</b>
-                        </div>
-                        <div className=" w-1/2 flex flex-col items-center">
-                            <b>Phone Number:</b>
-                            <b>{profile.phoneNumber}</b>
-                        </div>
-                      </div>
-                      <div className=" w-full flex flex-row justify-around items-center py-4 ">
-                        <div className=" w-1/2 flex flex-col items-center">
-                          <b>Birth Date:</b>
-                          <b>{profile.birthDate}</b>
-                        </div>
-                        <div className=" w-1/2 flex flex-col items-center">
-                            <b>Nationality:</b>
-                            <b>{profile.nationality}</b>
-                        </div>
-                      </div>
-                      <div className="flex justify-center py-2">
-                          <button className="bg-primary text-white py-2 px-4 rounded-md hover:bg-btn-hover">
-                            <Link to={`/profilefavs/${profile.name}`}><b>Edit Profile</b></Link>
-                          </button>
-                      </div>
-                      
-                    </div>
 
-                  </div>
-                </div>
+                <ProfileView
+                profile={profile}
+                />
               </div>
             </div>
           </div>
