688e578f5335d6c23158ae64381561cd5024e7cb
Dana Jacob Casanoves <danajacobc@gmail.com>
2024-01-24 11:32:56 -0300

    tourdetail buttons

--- client/src/views/TourDetail/Tour.jsx
+++ client/src/views/TourDetail/Tour.jsx
@@ -3,7 +3,6 @@ import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-
 // <-- Components -->
 import { getTourId } from "../../redux/tourStore/toursActions";
 import TourInfoItem from "../../components/TourInfoItem/TourInfoItem";
@@ -21,17 +20,27 @@ import iconTourDetail from "../../assets/icons/tourDetailicon.png";
 import IconIncludes from "../../assets/icons/includesIcon.png";
 import iconPhoto from "../../assets/icons/photosIcon.png";
 
-
-
 const Tour = () => {
   const tourDetail = useSelector((state) => state.tour.detail);
-  const guideDetail = useSelector((state) => state.guide.detail)
-  
+  const guideDetail = useSelector((state) => state.guide.detail);
+
   const initialDate = new Date(tourDetail.initialDate);
   const endDate = new Date(tourDetail.endDate);
 
   const differenceInMilliseconds = endDate - initialDate;
-  const differenceInDays = Math.ceil(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+  const differenceInDays = Math.ceil(
+    differenceInMilliseconds / (1000 * 60 * 60 * 24)
+  );
+
+  const scrollToSection = (event, sectionId) => {
+    event.preventDefault();
+
+    const targetSection = document.querySelector(sectionId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  };
 
   return (
     <main className=" font-Nunito">
@@ -47,14 +56,18 @@ const Tour = () => {
       <section className="px-4 pt-8 flex w-3/4">
         <div className="flex w-1/2">
           <ul>
-            <TourInfoItem icon={iconDay} label={"Duration: " + differenceInDays} />
+            <TourInfoItem
+              icon={iconDay}
+              label={"Duration: " + differenceInDays + " days."}
+            />
             <TourInfoItem
               icon={iconCalendar}
-              label={"Initial Date: " + tourDetail.initialDate} />
+              label={"Initial Date: " + tourDetail.initialDate}
+            />
             <TourInfoItem
               icon={iconCalendar}
-              label= {" End Date: " + tourDetail.endDate} />
-            
+              label={" End Date: " + tourDetail.endDate}
+            />
           </ul>
         </div>
         <div className="flex w-1/2">
@@ -75,28 +88,31 @@ const Tour = () => {
       {/*-------------- Page Nav Bar--------------*/}
       <section className="px-4 flex w-3/4 h-14 bg-gray-500 justify-around items-center">
         <div className="">
-          <button
-            type="button"
-            className="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800"
+          <a
+            href="#description"
+            onClick={(e) => scrollToSection(e, "#description")}
+            className="text-white hover:bg-green-800 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary dark:hover:bg-btn-hover dark:focus:ring-green-800"
           >
-            <Link to="#description">Description</Link>
-          </button>
+            Description
+          </a>
         </div>
         <div>
-          <button
-            type="button"
-            className="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800"
+          <a
+            href="#photos"
+            onClick={(e) => scrollToSection(e, "#photos")}
+            className="text-white hover:bg-green-800 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary dark:hover:bg-btn-hover dark:focus:ring-green-800"
           >
-            <Link to="#photos">Photos</Link>
-          </button>
+            Photos
+          </a>
         </div>
         <div>
-          <button
-            type="button"
-            className="focus:outline-none text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary  dark:hover:bg-btn-hover dark:focus:ring-green-800"
+          <a
+            href="#guide"
+            onClick={(e) => scrollToSection(e, "#guide")}
+            className="text-white hover:bg-green-800 font-medium rounded-full text-sm px-5 py-2.5 h-12 w-32 dark: bg-primary dark:hover:bg-btn-hover dark:focus:ring-green-800"
           >
-            <Link to="#guide">Guide</Link>
-          </button>
+            Guide
+          </a>
         </div>
       </section>
       {/*-------------- Page Nav Bar--------------*/}
@@ -117,9 +133,13 @@ const Tour = () => {
 
       {/*-------------- Second Image --------------*/}
       <section className=" w-3/4 px-4 flex justify-center">
-      {tourDetail && tourDetail.photos && tourDetail.photos.length > 0 && (
-    <img className=" h-[38rem] rounded-3xl my-4" src={tourDetail.photos[0]} alt="" />
-  )}
+        {tourDetail && tourDetail.photos && tourDetail.photos.length > 0 && (
+          <img
+            className=" h-[38rem] rounded-3xl my-4"
+            src={tourDetail.photos[0]}
+            alt=""
+          />
+        )}
       </section>
       {/*-------------- Detail tour --------------*/}
       <section className=" w-3/4">
@@ -129,7 +149,13 @@ const Tour = () => {
             <b>Depature place</b>
           </div>
           <div className="w-2/3 flex text-lg">
-            <p>{tourDetail.city + ', ' + tourDetail.country + ', ' + tourDetail.continent}</p>
+            <p>
+              {tourDetail.city +
+                ", " +
+                tourDetail.country +
+                ", " +
+                tourDetail.continent}
+            </p>
           </div>
         </div>
         <div className="bg-seconday-text h-2 my-4"></div>
@@ -138,7 +164,9 @@ const Tour = () => {
             <b>Travel time</b>
           </div>
           <div className="w-2/3 flex text-lg">
-            <p>{tourDetail.initialDate} - {tourDetail.endDate}</p>
+            <p>
+              {tourDetail.initialDate} - {tourDetail.endDate}
+            </p>
           </div>
         </div>
         <div className="bg-seconday-text h-2 my-4"></div>
@@ -186,20 +214,22 @@ const Tour = () => {
       </section>
       {/*-------------- Photos tour --------------*/}
       <section id="photos" className="flex-col px-4 w-3/4">
-      <PhotoSection
-        icon={iconPhoto}
-        title="Photos"
-        images={tourDetail.photos || []}
-      />
+        <PhotoSection
+          icon={iconPhoto}
+          title="Photos"
+          images={tourDetail.photos || []}
+        />
       </section>
       {/*-------------- Guide Section --------------*/}
-      <GuideSection
-        icon={iconTourDetail}
-        title="Guide"
-        guideName={guideDetail.forename + ' ' + guideDetail.surname}
-        guideImage={guideDetail.image}
-        guideDescription={guideDetail.biography}
-      />
+      <section id="guide" className="flex-col px-4">
+        <GuideSection
+          icon={iconTourDetail}
+          title="Guide"
+          guideName={guideDetail.forename + " " + guideDetail.surname}
+          guideImage={guideDetail.image}
+          guideDescription={guideDetail.biography}
+        />
+      </section>
     </main>
   );
 };
