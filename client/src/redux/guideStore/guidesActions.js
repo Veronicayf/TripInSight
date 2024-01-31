eefe8d4c6325ad861f45b935af5673d99fbb08b4
Dana Jacob Casanoves <danajacobc@gmail.com>
2024-01-31 00:22:55 -0300

    action delete guides

--- client/src/redux/guideStore/guidesActions.js
+++ client/src/redux/guideStore/guidesActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Swal from "sweetalert2/dist/sweetalert2.js";
 import "sweetalert2/dist/sweetalert2.min.css";
-import { getAllGuides, getGuideById, postGuideReducer } from './guidesSlice'
+import { deleteGuideReducer, getAllGuides, getGuideById, postGuideReducer, updateGuideReducer } from './guidesSlice'
 
 const URL = "http://localhost:4000"  //"https://tripinsight.onrender.com"
 
@@ -43,4 +43,34 @@ export const postGuide = (guideDate) => {
 
     }
   };
+};
+
+
+export const updateGuide = (guideData) => {
+  return async (dispatch) => {
+    try {
+      let response = await axios.put(
+        `${URL}/guides/updateguide`,
+        guideData
+      );
+      return dispatch(updateGuideReducer(response.data));
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+};
+
+export const deleteGuide = (guideId) => {
+  const url = `${URL}/guides/${guideId}`;
+  const deleteG = { guideId };
+  return async (dispatch) => {
+    try {
+      let { data } = await axios.delete(url, {
+        data: deleteG
+      });
+      return dispatch(deleteGuideReducer(data));
+    } catch (error) {
+      console.log(error.data);
+    }
+  }
 };
--- client/src/redux/userStore/usersActions.js
+++ client/src/redux/userStore/usersActions.js
@@ -131,3 +131,4 @@ export const getAllPurchased = () => {
    }
   }
 };
+
